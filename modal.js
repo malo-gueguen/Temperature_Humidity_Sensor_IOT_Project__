9dbd3e5cac15b5d85935ecdf65b3c320568cab9f
LucasRaoul <lucas.raoul.pro@gmail.com>
2025-03-13 10:25:11 +0100

    design modal et inscription

--- modal.js
+++ modal.js
@@ -25,8 +25,6 @@ btnConnexion.addEventListener("click", async function fetchConnexion (){
 
         }else{
             console.log("test")
-            document.getElementById("inputUser").style.backgroundColor = "red"
-            document.getElementById("inputPassword").style.backgroundColor = "red"
             document.getElementById("inputUser").value = ""
             document.getElementById("inputPassword").value = ""
         }
@@ -39,6 +37,7 @@ btnConnexion.addEventListener("click", async function fetchConnexion (){
 
 
 let sendButton = document.getElementById("loginButton")
+let inscription = document.getElementById("inscription")
 let state = 0;
 
 sendButton.addEventListener("click", function(){
@@ -50,32 +49,42 @@ sendButton.addEventListener("click", function(){
         console.log("modal open")
         containerModal.style.display = "flex"
         containerStation.style.filter = "blur(0.6rem)"
-    }else if (state == 1){
+    } else if (state == 1){
         state = 0
         console.log("modal close")
         containerModal.style.display = "none"
         containerStation.style.filter = "none"
-        
     }
 });
 
 
-document.addEventListener("click", function(e){
+// document.addEventListener("click", function(e){
+//     console.log("test")
+//     if (document.getElementById(containerModal).contains(e.target)){
+//         console.log("clic dans modal")
+//     }else{
+//         console.log("clic dehors modal")
+//         if(state == 1){
+//             state = 0
+//             containerModal.style.display = "none"
+//             containerStation.style.filter = "none"
+//         }else if (state == 0){
+//             state = 1
+//             containerModal.style.display = "flex"
+//             coinerStation.style.filter = "blur(0.6rem)"
+//         }
+//     }
+// });
+
+let SeConnecterh1 = document.getElementById("SeConnecterh1");
+let connexion = document.getElementById("connexion");
+
+
+inscription.addEventListener("click", function(e){
     console.log("test")
-    if (document.getElementById(containerModal).contains(e.target)){
-        console.log("clic dans modal")
-    }else{
-        console.log("clic dehors modal")
-        if(state == 1){
-            state = 0
-            containerModal.style.display = "none"
-            containerStation.style.filter = "none"
-        }else if (state == 0){
-            state = 1
-            containerModal.style.display = "flex"
-            coinerStation.style.filter = "blur(0.6rem)"
-        }
-    }
+    SeConnecterh1.innerHTML = "S'inscrire";
+    connexion.innerHTML = "S'inscrire";
+    inscription.innerHTML = "Se connecter"
 });
 
 
