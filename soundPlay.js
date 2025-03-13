e10a1a257556ef53ee158a6c49cbf7975f8d4bf3
Lilian Fischer <lilian.fischer@viacesi.fr>
2025-03-13 09:30:55 +0100

    Update & audio support

--- soundPlay.js
+++ soundPlay.js
@@ -1,10 +1,19 @@
 document.addEventListener("click", () => {
-    let randClicky = Math.random() * 50
+    let randClicky = Math.random() * 2
     let sound = NaN
     if (randClicky <= 1) {
-        sound = new Audio("OHYEAH.mp3");
+        let randIntern = Math.random() * 4
+        if (randIntern < 1) {
+            sound = new Audio("audioLck1.m4a");
+        } else if (randIntern < 2) {
+            sound = new Audio("audioLck2.m4a");
+        } else if (randIntern < 3) {
+            sound = new Audio("audioLck3.m4a");
+        } else {
+            sound = new Audio("audioLck4.m4a");
+        }
     } else {
-        sound = new Audio("MouseClickLoud.mp3");
+        sound = new Audio("audioDefault.m4a");
     }
     sound.play();
 });
