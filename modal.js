c08a1e26a169b13a7bac6b106ec25fa028eeeb76
Benjamin <pro.benjamin.leon@gmail.com>
2025-03-13 10:26:28 +0100

    changement lors de la connexion

--- modal.js
+++ modal.js
@@ -1,7 +1,9 @@
 
 
 
-let btnConnexion = document.getElementById("connexion");
+let btnConnexion = document.getElementById("connexion")
+let containerStation = document.getElementById("containerStation")
+let containerModal = document.getElementById("containerModal")
 btnConnexion.addEventListener("click", async function fetchConnexion (){
     try {
         const response = await fetch("https://iotcesi.alwaysdata.net/user_conn.php", {
@@ -15,16 +17,17 @@ btnConnexion.addEventListener("click", async function fetchConnexion (){
     
         data.forEach((item) => {
 
-        if( inputUser == item.user && inputPassword == item.password) {
-            console.log("done")
+        if( inputUser == item.user && inputPassword == item.password) {            
             
             document.getElementById("inputUser").style.backgroundColor = "bisque"
             document.getElementById("inputPassword").style.backgroundColor = "bisque"
             document.getElementById("inputUser").value = ""
             document.getElementById("inputPassword").value = ""
+            document.getElementById("nbDataSelector").classList.remove("hidden")
+            closeModal(state, containerModal, containerStation)
+
 
         }else{
-            console.log("test")
             document.getElementById("inputUser").style.backgroundColor = "red"
             document.getElementById("inputPassword").style.backgroundColor = "red"
             document.getElementById("inputUser").value = ""
@@ -39,44 +42,54 @@ btnConnexion.addEventListener("click", async function fetchConnexion (){
 
 
 let sendButton = document.getElementById("loginButton")
-let state = 0;
+let state = 0
 
-sendButton.addEventListener("click", function(){
+sendButton.addEventListener("click", () =>{
     console.log("modal")
-    let containerStation = document.getElementById("containerStation")
-    let containerModal = document.getElementById("containerModal")
     if(state == 0){
+        openModal(state, containerModal, containerStation)
+    }else if (state == 1){
+        closeModal(state, containerModal, containerStation)
+    }
+});
+
+
+function openModal(state, containerModal, containerStation){
         state = 1
         console.log("modal open")
         containerModal.style.display = "flex"
         containerStation.style.filter = "blur(0.6rem)"
-    }else if (state == 1){
+        return state
+}
+
+function closeModal(state, containerModal, containerStation){
         state = 0
         console.log("modal close")
         containerModal.style.display = "none"
-        containerStation.style.filter = "none"
-        
-    }
-});
+        containerStation.style.filter = "none" 
+        return state
+}
 
 
-document.addEventListener("click", function(e){
-    console.log("test")
-    if (document.getElementById(containerModal).contains(e.target)){
-        console.log("clic dans modal")
-    }else{
-        console.log("clic dehors modal")
-        if(state == 1){
-            state = 0
-            containerModal.style.display = "none"
-            containerStation.style.filter = "none"
-        }else if (state == 0){
-            state = 1
-            containerModal.style.display = "flex"
-            coinerStation.style.filter = "blur(0.6rem)"
-        }
-    }
-});
+// document.addEventListener("click", (event) => {
+//     console.log("test")
+//     if (document.getElementById("containerModal").contains(event.target)){
+//         console.log("clic dans modal")
+//     }else{
+//         console.log("clic dehors modal")
+//         if(state == 1){
+//             state = 0
+//             containerModal.style.display = "none"
+//             containerStation.style.filter = "none"
+//         }else if (state == 0){
+//             state = 1
+//             containerModal.style.display = "flex"
+//             containerStation.style.filter = "blur(0.6rem)"
+//         }
+//     }
+// });
+
+
 
 
 // function changeForm(){
