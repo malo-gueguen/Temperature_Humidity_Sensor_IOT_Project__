c49c006d1fb3bf7b1a141449cfebb7b84a90b1a8
Benjamin <pro.benjamin.leon@gmail.com>
2025-03-17 22:51:08 +0100

    vérif doublons db

--- AddUser.php
+++ AddUser.php
@@ -1,8 +1,11 @@
 <?php 
 //décoder le json reçu
+
     include 'db_conn.php';
+
     $json= file_get_contents("php://input");
     $data=json_decode($json, true);
+
     if ($data) {
             echo json_encode(["message" => "Données reçues", "data" => $data]);
         } else {
@@ -10,15 +13,25 @@
         }
 
 //vérifier doublons
-    
+
+    $user = htmlspecialchars($data["user"]);
+    $password = htmlspecialchars($data["password"]);
+
+    $count = $pdo->prepare("SELECT COUNT(*) FROM Users WHERE user = :inputUser");
+    $count->bindParam(':inputUser', $user, PDO::PARAM_STR);
+    $count->execute();
+    $numdoublon = $count->fetchColumn();
 
 // créer nouvel user
-    if ($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST" ){
+
+        if($numdoublon != 0){
+            echo ("Vous avez déjà un compte au nom de ". $user);
+            return;
+        }
+
         try{
-            $user = htmlspecialchars($data["user"]);
-            echo ($user);
-            $password = htmlspecialchars($data["password"]);
-            echo ($password);
             $request = $pdo->prepare("INSERT INTO Users (user, password) VALUES (:user, :password)");
             $request->bindParam(':user', $user,PDO::PARAM_STR);       
             $request->bindParam(':password', $user, PDO::PARAM_STR);  
@@ -27,5 +40,8 @@
             http_response_code(500);
             echo json_encode(["error" => "Failed to load data: " . $e->getMessage()]);
         }
+
     }
+
 ?>
+   
